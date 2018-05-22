422fafb6d54c460c32fa713a0fe0c21fca513fd7
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-05-22 23:54:05 +0300

    Qualifier annotation

--- di-demo/src/main/java/com/sturc/DiDemoApplication.java
+++ di-demo/src/main/java/com/sturc/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package com.sturc;
 
+import com.sturc.controllers.ConstructorInjectedController;
+import com.sturc.controllers.GetterInjectedController;
 import com.sturc.controllers.MyController;
+import com.sturc.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,5 +17,9 @@ public class DiDemoApplication {
 		MyController controller = (MyController) ctx.getBean("myController");
 
 		controller.hello();
+
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
 }
---
+++ di-demo/src/main/java/com/sturc/services/ConstructorGreetingService.java
@@ -0,0 +1,12 @@
+package com.sturc.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello - injected by the constructor";
+    }
+}
